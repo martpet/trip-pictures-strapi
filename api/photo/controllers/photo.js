34c1096bbe598eef4ab1ab0ca87ec80fab6870d9
Martin Petrov <martin_petrov@me.com>
2022-03-05 11:09:47 +0200

    upload wip

--- api/photo/controllers/photo.js
+++ api/photo/controllers/photo.js
@@ -5,19 +5,23 @@ const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
   async create(ctx) {
-    const entity = await strapi.services.photo.create({
-      ...ctx.request.body,
-      user: ctx.state.user.id,
-    });
-
-    return sanitizeEntity(entity, { model: strapi.models.photo });
+    const entities = await Promise.all(
+      ctx.request.body.map((item) =>
+        strapi.services.photo.create({
+          ...item,
+          user: ctx.state.user.id,
+        }),
+      ),
+    );
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.photo }),
+    );
   },
 
   async createPresignedUploadUrls(ctx) {
     const { uploadsLength } = ctx.request.body;
     const { user } = ctx.state;
     const promises = [];
-
     const s3Client = new S3Client({
       region: 'eu-central-1',
       credentials: {
@@ -25,14 +29,13 @@ module.exports = {
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       },
     });
-
     for (let i = 0; i < uploadsLength; i++) {
       const s3uuid = uuidv4();
       promises.push(
         new Promise((resolve) => {
           createPresignedPost(s3Client, {
             Bucket: process.env.S3_UPLOAD_BUCKET,
-            Key: `${user.id}/${s3uuid}.jpg`,
+            Key: createS3Key(user.id, s3uuid),
             Fields: { 'Content-Type': 'image/jpeg' },
             Conditions: [['content-length-range', 0, 50 * 1024 * 1024]],
           }).then((presignedPost) => {
@@ -44,7 +47,10 @@ module.exports = {
         }),
       );
     }
-
     return Promise.all(promises);
   },
 };
+
+function createS3Key(userId, uuid) {
+  return `${userId}/${uuid}.jpg`;
+}
