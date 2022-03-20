167cf225617f188c4513c876e6aa6098f9891802
Martin Petrov <martin_petrov@me.com>
2022-03-20 14:00:48 +0200

    Fix  converting photo url to base64

--- api/photo/services/photo.js
+++ api/photo/services/photo.js
@@ -2,11 +2,12 @@ const { imageHandlerEndpoint } = require('../../../consts');
 
 module.exports = {
   getUrl(userId, s3uuid) {
-    const imageRequest = JSON.stringify({
+    const data = {
       bucket: process.env.S3_UPLOAD_BUCKET,
       key: strapi.services.photo.getS3Key(userId, s3uuid),
-    });
-    return `${imageHandlerEndpoint}/${Buffer.from(imageRequest, 'base64')}`;
+    };
+    const encodedData = Buffer.from(JSON.stringify(data), 'utf8').toString('base64');
+    return `${imageHandlerEndpoint}/${encodedData}`;
   },
 
   getS3Key(userId, s3uuid) {
